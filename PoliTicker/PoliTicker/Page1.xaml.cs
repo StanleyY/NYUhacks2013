43c20e43723f7836d8ef0e1e95c04475e8c4271d
Stanley <stanley.yang.sy@gmail.com>
2013-11-16 21:29:15 -0500

    work so far

---
+++ PoliTicker/PoliTicker/Page1.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace PoliTicker
+{
+    public partial class Page1 : PhoneApplicationPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void gotoGovernor(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("/Page2.xaml", UriKind.Relative));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ PoliTicker/PoliTicker/Page2.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using System.Windows.Threading;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace PoliTicker
+{
+    public partial class Page2 : PhoneApplicationPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void ApprovalChange(object sender, RoutedEventArgs e)
+        {
+            p1.Offset = 0.5;
+            p2.Offset = 0.5;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
